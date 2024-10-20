42457967b6223cc2dbc53a0589f5ce4ecc9e906f
YOUWILLDIE666 <youwilldie6.066@gmail.com>
2024-10-20 13:44:54 +0300

    fix base address finding (real), general optimization * optimize imports * reduce write_header() function params * implement exception handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Copyright (c) YOUWILLDIE666.
-# Licensed under the GPL-3.0 license. See LICENSE.txt file for full license information.
+# Licensed under the GPL-3.0 license. See LICENSE.txt file in the project root for full license information.
 
 ########################################################################
 #  main.py - dumps offsets based on the address of the first function  #
@@ -8,21 +8,19 @@
 
 import idautils, idaapi, idc
 import tkinter as tk
-from tkinter import filedialog
 import os
 
 # Constants
 COMMENT_HEADER = "/*\n * Base Address: {}\n */\n"
-BASE_ADDRESS = None
-for func in idautils.Functions():
-    BASE_ADDRESS = func
-    break
+BASE_ADDRESS = next(idautils.Functions(), None)
 
-def write_header(file, base_name, is_header, is_header_pp): # i know this looks strange
-    if is_header:
-        file.write(f"#ifndef {base_name}_H\n#define {base_name}_H\n\n")
-    elif is_header_pp:
-        file.write("#pragma once\n\n")
+def write_header(file, base_name, is_header_int):
+    hstr_map = {
+        1: f"#ifndef {base_name}_H\n#define {base_name}_H\n\n",
+        2: "#pragma once\n\n"
+    }
+    hstr = hstr_map.get(is_header_int, "")
+    file.write(hstr)
     file.write("#include <stdint.h>\n\n")
     file.write("const static uint32_t function_offsets[] = {\n")
 
@@ -30,7 +28,7 @@ def write_offsets(file, ea_list, comment_prefix):
     for ea in ea_list:
         name = idc.get_func_name(ea) if comment_prefix == 'function' else idc.get_name(ea)
         offset = ea - BASE_ADDRESS
-        if offset > 0 and offset < 0x7fffffff and name != None: # or 0xffffff i don't really know
+        if offset > 0 and name:
             file.write(f"    {hex(offset)}, // {name}\n")
 
 def write_data_offsets(file):
@@ -47,36 +45,48 @@ def write_data_offsets(file):
 def dump_offsets(filename):
     base_name = os.path.splitext(os.path.basename(filename))[0].replace(' ', '_').upper()
     file_ext = filename.split('.')[-1]
-
-    is_header = file_ext in {'h', 'hpp'}
-    is_header_pp = file_ext == 'hpp'
+    h_map = {
+        'h': 1,
+        'hpp': 2
+    }
+    
+    is_header_int = h_map.get(file_ext, 0)
+    is_header_bool = file_ext in {'h', 'hpp'}
     is_cpp = file_ext in {'cpp', 'cxx', 'cc', 'c++'}
 
-    if not (is_header or is_cpp or is_header_pp):
+    if not (is_header_bool or is_cpp):
         print("Unsupported file extension. Defaulting to C++")
         is_cpp = True
 
-    with open(filename, 'w') as file:
-        file.write(COMMENT_HEADER.format(BASE_ADDRESS))
-        write_header(file, base_name, is_header, is_header_pp)
-        write_offsets(file, idautils.Functions(), 'function')
-        file.write("};\n\n")
-        write_data_offsets(file)
-        if is_header:
-            file.write(f"\n#endif // {base_name}_H")
+    try:
+        with open(filename, 'w') as file:
+            file.write(COMMENT_HEADER.format(hex(BASE_ADDRESS)))
+            write_header(file, base_name, is_header_int)
+            write_offsets(file, idautils.Functions(), 'function')
+            file.write("};\n\n")
+            write_data_offsets(file)
+            if is_header_bool:
+                file.write(f"\n#endif // {base_name}_H")
+    except Exception as e:
+        print(e)
 
 def select_file() -> str:
     root = tk.Tk()
     root.withdraw()
-    return filedialog.asksaveasfilename(
+    return tk.filedialog.asksaveasfilename(
         defaultextension=".cpp",
-        filetypes=[("C++ Files", ("*.cpp", "*.hpp")), ("C Files", ("*.c", ".h")), ("All Files", "*.*")]
+        filetypes=[
+            ("C++ Files", ("*.cpp", "*.hpp")),
+            ("C Files", ("*.c", ".h")),
+            ("All Files", "*.*")
+        ]
     )
 
-output_file = select_file()
+if __name__ == "__main__":
+    output_file = select_file()
 
-if output_file:
-    dump_offsets(output_file)
-    print(f"Offsets have been written to {output_file}")
-else:
-    print("No file selected. Offsets not written.")
+    if output_file:
+        dump_offsets(output_file)
+        print(f"Offsets have been dumped to {output_file}")
+    else:
+        print("No file selected. Offsets not dumped.")
